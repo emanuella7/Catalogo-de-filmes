b4679edf18a032d6c948d5b1d82d473851c75d68
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-11-29 19:02:22 -0300

    Delete Web directory

--- Web/app/models/Pessoa.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Filme(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
-    ano = db.Column(db.Integer, nullable=False)
-    diretor = db.Column(db.String(100), nullable=False)
-    def __repr__(self):
-        return f"<Filme {self.titulo} ({self.ano})>"
