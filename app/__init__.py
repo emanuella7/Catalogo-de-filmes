a0c7b8da40915cf3b1b743b3df717bb612e01d96
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-11-29 21:51:00 -0300

    Refactor create_app function and database setup

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, static_folder="static", template_folder="templates")
 
-    # Configurações
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    # banco relativo à raiz do projeto (WEB/filmes.db)
+  
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, '..', 'filmes.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, '..', 'static', 'uploads')
@@ -17,12 +16,11 @@ def create_app():
 
     db.init_app(app)
 
-    # Importar modelos (para registro do SQLAlchemy)
-    with app.app_context():
-        from app.models.Filme import Filme  # importa o modelo
+   
+    with app.app_context(): # importa o modelo
         db.create_all()
 
-    # Registrar rotas definidas em controllers usando app.route
+  
     from app.controllers import views
     views.register_routes(app)
 
@@ -40,3 +38,4 @@ def create_app():
     with app.app_context():
         db.create_all()
     return app
+
